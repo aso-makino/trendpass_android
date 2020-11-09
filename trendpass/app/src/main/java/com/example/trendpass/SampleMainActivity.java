6aecc3b49c7022c5d868a52ac97e47f9cb193b09
kinoshita <1801156@s.asojuku.ac.jp>
2020-11-09 09:40:04 +0900

    footer作りました

--- trendpass/app/src/main/java/com/example/trendpass/SampleMainActivity.java
+++ trendpass/app/src/main/java/com/example/trendpass/SampleMainActivity.java
@@ -2,10 +2,14 @@ package com.example.trendpass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.trendpass.async.SampleAsyncActivity;
 
@@ -26,10 +30,38 @@ public class SampleMainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        findViewById(R.id.mapbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("mapが押されました");
+            }
+        });
+
+        findViewById(R.id.insertbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("insertが押されました");
+            }
+        });
+
+        findViewById(R.id.listbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("listが押されました");
+            }
+        });
+
+        findViewById(R.id.userbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("userが押されました");
+            }
+        });
+
         Button sendBtn = (Button) findViewById(R.id.button);
         sendBtn.setOnClickListener(new View.OnClickListener(){
 
-            public void onClick(View v){
+            public void onClick(View view){
 
                 EditText eText = (EditText) findViewById(R.id.etword);
                 String word = eText.getText().toString();
